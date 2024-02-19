e2ccf8abd7948a77ae7c5bad556edb135d997607
Acksout <157364563+Acksout@users.noreply.github.com>
2024-02-19 14:57:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import env from "vite-plugin-env";
 
 export default defineConfig({
     plugins: [
         react(),
-        env({
-            // Path to your .env file (relative to project root)
-            file: ".env",
-        })
-    ]
+    ],
+    envFile: '.env', // specify the .env file here
 });
